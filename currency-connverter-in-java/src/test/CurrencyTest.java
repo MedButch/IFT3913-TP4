c6ccb74a32b4e541a645161eb7480073c9971a65
MedButch <medebouchard@gmail.com>
2023-12-06 22:36:09 -0500

    Currency.convert black box test cases

--- currency-connverter-in-java/src/test/CurrencyTest.java
+++ currency-connverter-in-java/src/test/CurrencyTest.java
@@ -1,7 +1,15 @@
 package test;
 
+import currencyConverter.Currency;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CurrencyTest {
 
+    @Test
+    void convert(){
+        assertEquals(Currency.convert());
+    }
 }
